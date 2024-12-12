b1cb4e051c07f6252540fca26d26781fd3ed6315
bohyemian <tomboy.kr@gmail.com>
2024-12-12 09:38:12 +0900

    Chore: 이미지 outDir 경로 변경
    - 배포 이미지 파일 폴더로 구분되도록 설정 변경
    
    #8

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,24 @@
 // import { resolve } from 'node:path'
 import { defineConfig } from 'vite';
+import { resolve } from 'node:path';
 
 export default defineConfig({
   build: {
     outDir: 'docs',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        // login: resolve(__dirname, 'src/login.html'),
+      },
+      output: {
+        outDir: (folder) => {
+          if (folder.name.includes('assets/')) {
+            return 'assets/[name][extname]';
+          }
+          return '[name][extname]';
+        },
+      },
+    },
   },
   resolve: {
     alias: [{ find: '@', replacement: '/src' }],

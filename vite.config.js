efac1ec1a891d41d00354079cf08c68c392c5ac6
Min-gyu <jorushs@gmail.com>
2024-12-12 18:06:59 +0900

    Chore : vite.config.js 수정
    - rollupOption 추가

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 // import { resolve } from 'node:path'
 import { defineConfig } from 'vite';
+import { resolve } from 'node:path';
 
 export default defineConfig({
   build: {
     outDir: 'docs',
+    rollupOptions: {
+      input: {
+        login: resolve(__dirname, 'src/pages/login/index.html'),
+      },
+    },
   },
   resolve: {
     alias: [{ find: '@', replacement: '/src' }],

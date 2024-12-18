ea055f02f34e6239f8ff27344c0519b1b319dcf3
Min-gyu <jorushs@gmail.com>
2024-12-18 16:07:46 +0900

    Feat: checkbox 컴포넌트 제작
    - t-checkbox 컴포넌트 제작
    - 반응형 구현

---
+++ src/components/Checkbox.ts
@@ -0,0 +1,100 @@
+import { html, css, CSSResultGroup } from 'lit';
+import { customElement } from 'lit/decorators.js';
+import { TaingElement } from './Taing';
+import { property } from 'lit/decorators.js';
+
+@customElement('t-checkbox')
+export class Checkbox extends TaingElement {
+  @property({ type: String }) id = 'default';
+  @property({ type: Boolean }) checked = false;
+
+  static styles: CSSResultGroup = [
+    super.styles,
+    css`
+      .checkbox-container {
+        --icon-width: 1rem;
+        --icon-height: 1rem;
+        --font-size: 0.7502rem;
+
+        @media (min-width: 48rem) {
+          --icon-width: 1rem;
+          --icon-height: 1rem;
+          --font-size: 0.7502rem;
+        }
+
+        @media (min-width: 120rem) {
+          --icon-width: 1.625rem;
+          --icon-height: 1.625rem;
+          --font-size: 1.333rem;
+        }
+
+        box-sizing: border-box;
+        display: flex;
+        align-items: flex-start;
+        white-space: nowrap;
+        padding: 0;
+      }
+
+      .checkbox {
+        position: absolute;
+        opacity: 0;
+      }
+
+      .checkbox-label {
+        display: flex;
+        flex-flow: row nowrap;
+        align-items: center;
+        color: var(--gray500);
+        font-size: var(--font-size);
+        font-weight: 600;
+        line-height: 150%;
+      }
+
+      .icon-check {
+        display: inline-block;
+        margin-right: 0.25rem;
+        background-image: url('/assets/images/icon/checkbox.svg');
+        background-size: cover;
+        background-position: center;
+        inline-size: var(--icon-width);
+        block-size: var(--icon-height);
+      }
+
+      .checkbox:checked + .checkbox-label .icon-check {
+        background-image: url('/assets/images/icon/checkbox_checked.svg');
+      }
+    `,
+  ];
+
+  handleChange(event: Event) {
+    const target = event.target as HTMLInputElement;
+    const checked = target.checked;
+
+    this.dispatchEvent(
+      new CustomEvent('change', {
+        detail: { checked },
+        bubbles: true,
+        composed: true,
+      })
+    );
+  }
+
+  render() {
+    return html`
+      <div class="checkbox-container">
+        <input
+          type="checkbox"
+          class="checkbox"
+          id=${this.id}
+          name="state"
+          ?checked=${this.checked}
+          @change=${this.handleChange}
+        />
+        <label for=${this.id} class="checkbox-label">
+          <span class="icon-check"></span>
+          <slot></slot>
+        </label>
+      </div>
+    `;
+  }
+}

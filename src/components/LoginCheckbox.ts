b6a2c46cc3d749bc4eaccfcfec8f6537b547139f
Min-gyu <jorushs@gmail.com>
2024-12-19 09:56:55 +0900

    Refactor: 로그인 체크박스 리팩토링
    - 로그인 페이지와 회원가입 페이지에서 사용할 체크박스
    - Checkbox를 상속받도록 리팩토링
    - 컨테이너 반응형 구현
    
    #2

--- src/components/LoginCheckbox.ts
+++ src/components/LoginCheckbox.ts
@@ -1,75 +1,27 @@
-import { html, css, CSSResultGroup } from 'lit';
+import { css, CSSResultGroup } from 'lit';
 import { customElement } from 'lit/decorators.js';
-import { TaingElement } from './Taing';
-import { property } from 'lit/decorators.js';
+import { Checkbox } from './Checkbox';
 
 @customElement('login-checkbox')
-export class LoginCheckbox extends TaingElement {
-  @property({ type: String }) id = 'default';
-  @property({ type: Boolean }) checked = false;
-  @property({ type: String }) label = '';
-
+export class LoginCheckbox extends Checkbox {
   static styles: CSSResultGroup = [
     super.styles,
     css`
-      .checkbox-wrap {
-        --wrap-width: 18rem;
-        --wrap-height: 2.6875rem;
-        --wrap-padding: 0.5rem 0rem 1rem 0rem;
-        --icon-width: 1rem;
-        --icon-height: 1rem;
-        --font-size: 0.7502rem;
+      .checkbox-container {
+        --container-padding: 0.5rem 0rem 1rem 0rem;
 
         @media (min-width: 48rem) {
-          --wrap-width: 26rem;
-          --wrap-height: 2.875rem;
-          --wrap-padding: 0.75rem 0 1rem 0;
-          --icon-width: 1rem;
-          --icon-height: 1rem;
-          --font-size: 0.7502rem;
+          --container-padding: 0.75rem 0 1rem 0;
         }
 
         @media (min-width: 120rem) {
-          --wrap-width: 45.75rem;
-          --wrap-height: 5rem;
-          --wrap-padding: 1rem 0 2rem 0;
-          --icon-width: 1.625rem;
-          --icon-height: 1.625rem;
-          --font-size: 1.333rem;
+          --container-padding: 1rem 0 2rem 0;
         }
-
-        box-sizing: border-box;
-        display: flex;
-        align-items: flex-start;
-        white-space: nowrap;
-        inline-size: var(--wrap-width);
-        block-size: var(--wrap-height);
-        padding: var(--wrap-padding);
-      }
-
-      .checkbox {
-        position: absolute;
-        opacity: 0;
-      }
-
-      .checkbox-label {
-        display: flex;
-        flex-flow: row nowrap;
-        align-items: center;
-        color: var(--gray500);
-        font-size: var(--font-size);
-        font-weight: 600;
-        line-height: 150%;
+        padding: var(--container-padding);
       }
 
       .icon-check {
-        display: inline-block;
-        margin-right: 0.25rem;
         background-image: url('/assets/images/icon/login_checkbox.svg');
-        background-size: cover;
-        background-position: center;
-        inline-size: var(--icon-width);
-        block-size: var(--icon-height);
       }
 
       .checkbox:checked + .checkbox-label .icon-check {
@@ -78,34 +30,7 @@ export class LoginCheckbox extends TaingElement {
     `,
   ];
 
-  handleChange(event: Event) {
-    const target = event.target as HTMLInputElement;
-    const checked = target.checked;
-
-    this.dispatchEvent(
-      new CustomEvent('change', {
-        detail: { checked },
-        bubbles: true,
-        composed: true,
-      })
-    );
-  }
-
   render() {
-    return html`
-      <div class="checkbox-wrap">
-        <input
-          type="checkbox"
-          class="checkbox"
-          id=${this.id}
-          name="state"
-          ?checked=${this.checked}
-          @change=${this.handleChange}
-        />
-        <label for=${this.id} class="checkbox-label">
-          <span class="icon-check"></span>${this.label}
-        </label>
-      </div>
-    `;
+    return super.render();
   }
 }

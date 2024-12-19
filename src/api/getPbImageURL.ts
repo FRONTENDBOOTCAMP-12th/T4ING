668515d845c49a2f1854184fd9aa70819dbd6680
hunzooyun <hunzooyun@gmail.com>
2024-12-19 16:08:05 +0900

    Modify: conflict 수정
    - package-lock.json 수정
    -getPbImageURL수정

--- src/api/getPbImageURL.ts
+++ src/api/getPbImageURL.ts
@@ -1,7 +1,13 @@
-import { Item } from '../@types/type';
+import { BannerItem, RecommendItem } from '../@types/type';
 
-export function getPbImageURL(item: Item) {
-  return `${import.meta.env.VITE_PB_API}/files/${item.collectionId}/${
-    item.id
-  }/${item.img}`;
+export function getBannerImageURL(item: BannerItem) {
+  return `${import.meta.env.VITE_PB_API}/files/main_banner/${item.id}/${
+    item.img || 'default.jpg'
+  }`;
+}
+
+export function getRecommendImageURL(item: RecommendItem) {
+  return `${import.meta.env.VITE_PB_API}/files/main_recommend/${item.id}/${
+    item.img || 'default.jpg'
+  }`;
 }

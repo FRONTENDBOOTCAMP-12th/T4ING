147f3e9fb03e322c61001f4c5a37896588da0920
혜미 <tomboy.kr@gmail.com>
2024-12-20 01:36:02 +0900

    Modify: 가이드에 체크박스 추가
    -  <login-checkbox>, <t-checkbox> 추가
    
    #1

--- guide/guide.ts
+++ guide/guide.ts
@@ -1,9 +1,11 @@
-import { html, CSSResultGroup, css } from 'lit';
+import { html, CSSResultGroup } from 'lit';
 import { customElement } from 'lit/decorators.js';
 import { TaingElement } from '../src/components/Taing';
 import { buttonCSS } from '../src/styles/buttonCSS';
 import '/src/components/SvgIcon';
 import '/src/components/Form';
+import '/src/components/Checkbox';
+import '/src/components/LoginCheckbox';
 import '/src/components/Button';
 import '/main.ts';
 

0b725b052e27b9385b74242ef96c7a26759d06d1
Min-gyu <jorushs@gmail.com>
2024-12-19 18:46:53 +0900

    Refactor: 로그인 컴포넌트 리팩토링
    - 컴포넌트로 구성되도록 리팩토링
    - 반응형 구현
    
    #2

--- src/styles/loginCSS.ts
+++ src/styles/loginCSS.ts
@@ -1,97 +1,54 @@
 import { css } from 'lit';
+import { register } from 'swiper/element';
 
 export default css`
-  .login {
-    --button-color: var(--red-1);
-    inline-size: 100%;
-    block-size: 100%;
-    padding-top: 2.375rem;
-  }
-  .login-title {
-    display: flex;
-    justify-content: center;
-    align-items: flex-start;
-    align-self: stretch;
-    color: var(--white);
-    font-weight: 700;
-    line-height: 140%;
-    margin-bottom: 1.25rem;
-  }
-  .login__input {
-    margin-top: 0.5rem;
+  .login-container {
+    --transition: 0.3s;
+    --container-size: 19.5625rem;
+    --container-padding-top: 2.375rem;
+    --title-margin-bottom: 1.25rem;
+    --title-font-size: var(--text-size-m);
+    --input-gap: 0.5rem;
+    --find-account-wrap-padding-top: 1.25rem;
+    --font-size: var(--text-size-s);
+    --find-acount-padding: 0rem 0.75rem;
+    --hr-height: 0.75rem;
+    --hr-margin: 0 0.5rem;
+    --login-register-wrap-padding-top: 1.25rem;
+    --login-register-padding: 0 0.75rem;
 
-    & input {
-      display: flex;
-      width: 18rem;
-      height: 2.875rem;
-      padding: 0rem 1.25rem 0rem 1rem;
-      align-items: center;
-      color: var(--gray600);
-      line-height: 160%;
-      font-weight: 400;
-      border-radius: 0.25rem;
-      background: var(--dark-bg-2);
-      border: none;
-    }
-  }
-  .login__state-checkbox-wrap {
-    box-sizing: border-box;
-    display: flex;
-    color: var(--gray500);
-    inline-size: 18rem;
-    block-size: 2.6875rem;
-    padding: 0.5rem 0rem 1rem 0rem;
-    align-items: flex-start;
-    white-space: nowrap;
-    font-size: 0.7502rem;
-    font-style: normal;
-    font-weight: 600;
-    line-height: 150%;
-
-    .login__state-checkbox {
-      position: absolute;
-      flex-shrink: 0;
-      visibility: hidden;
+    @media (min-width: 48rem) {
+      --container-size: 23.25rem;
+      --container-padding-top: 2.5rem;
+      --title-margin-bottom: 1.5rem;
+      --title-font-size: var(--text-size-l);
+      --input-gap: 0.5rem;
+      --find-account-wrap-padding-top: 1.5rem;
+      --font-size: var(--text-size-m);
+      --find-acount-padding: 0rem 1rem;
+      --hr-height: 0.875rem;
+      --hr-margin: 0 0.5rem;
+      --login-register-wrap-padding-top: 1rem;
+      --login-register-padding: 0 0.75rem;
     }
 
-    .login__state-label {
-      display: flex;
-      flex-flow: row nowrap;
-      align-items: center;
+    @media (min-width: 120rem) {
+      --container-size: 45.75rem;
+      --container-padding-top: 4.375rem;
+      --title-margin-bottom: 3.75rem;
+      --title-font-size: var(--text-size-xl);
+      --input-gap: 0.9375rem;
+      --find-account-wrap-padding-top: 3rem;
+      --font-size: var(--text-size-l);
+      --find-acount-padding: 0rem 2.4rem;
+      --hr-height: 1.125rem;
+      --hr-margin: 0 0.75rem;
+      --login-register-wrap-padding-top: 2.75rem;
+      --login-register-padding: 0 1rem;
     }
 
-    .icon-check {
-      display: inline-block;
-      margin-right: 0.25rem;
-      inline-size: 1rem;
-      block-size: 1rem;
-      background-image: url('/assets/images/icon/login_checkbox.svg');
-      background-size: cover;
-      background-position: center;
-    }
-
-    .login__state-checkbox:checked + .login__state-label .icon-check {
-      background-image: url('/assets/images/icon/login_checkbox_checked.svg');
-    }
-  }
-
-  .login__button {
-    display: flex;
-    inline-size: 18rem;
-    block-size: 2.625rem;
-    justify-content: center;
-    align-items: center;
-    border: none;
-    background: var(--button-color);
-    padding: 1rem;
-    cursor: pointer;
-    inline-size: 100%;
-
-    color: var(--white);
-    font-size: 0.7502rem;
-    font-style: normal;
-    font-weight: 600;
-    line-height: 150%;
+    justify-self: center;
+    padding-top: var(--container-padding-top);
   }
 
   .login-wrap {
@@ -100,46 +57,64 @@ export default css`
     align-items: center;
     flex: 1 0 0;
   }
+  .login__title {
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+    color: var(--white);
+    font-size: var(--title-font-size);
+    font-weight: 700;
+    line-height: 140%;
+    margin-bottom: var(--title-margin-bottom);
+    transition: var(--transition);
+  }
+  .login__input-wrap {
+    display: flex;
+    flex-direction: column;
+    gap: var(--input-gap);
+    transition: var(--transition);
+  }
 
   .login__register-wrap {
-    margin-top: 1.25rem;
+    padding-top: var(--login-register-wrap-padding-top);
     color: var(--gray500);
-    font-size: 0.7502rem;
+    font-size: var(--font-size);
     font-weight: 400;
     line-height: 160%;
-
+    transition: var(--transition);
     .login__register {
-      padding: 0.75rem;
       color: var(--gray300);
+      padding: var(--login-register-padding);
     }
   }
 
   .login__find-acount-wrap {
     display: flex;
-    justify-self: center;
-    width: 18rem;
-    padding-top: 1.25rem;
     justify-content: center;
+    padding-top: var(--find-account-wrap-padding-top);
     align-items: center;
     gap: 0.5rem;
     font-weight: 400;
+    transition: var(--transition);
 
     & hr {
-      margin: 0 0.5rem;
+      margin: var(--hr-margin);
       width: 0.0625rem;
-      height: 0.75rem;
+      height: var(--hr-height);
       border: none;
       background: var(--gray800);
+      transition: var(--transition);
     }
 
     & a {
       display: flex;
-      padding: 0rem 0.75rem;
+      padding: var(--find-acount-padding);
       align-items: flex-start;
       color: var(--gray300);
-      font-size: 0.7502rem;
+      font-size: var(--font-size);
       font-weight: 400;
       line-height: 160%;
+      transition: var(--transition);
     }
   }
 `;

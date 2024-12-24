58c9a972640333efcad626240f782b5003519ebf
혜미 <tomboy.kr@gmail.com>
2024-12-25 00:22:51 +0900

    Feat: 모달 팝업 추가
    - 모달 추가
    
    #19

--- guide/guide.ts
+++ guide/guide.ts
@@ -1,4 +1,4 @@
-import { html, CSSResultGroup } from 'lit';
+import { html, CSSResultGroup, css } from 'lit';
 import { customElement } from 'lit/decorators.js';
 import { TaingElement } from '../src/components/Taing';
 import { buttonCSS } from '../src/styles/buttonCSS';
@@ -7,6 +7,7 @@ import '../src/components/SvgIcon';
 import '../src/components/Form';
 import '../src/components/Checkbox';
 import '../src/components/Button';
+import '../src/components/Modal';
 import '../main.ts';
 
 @customElement('guide-logo')
@@ -61,6 +62,126 @@ class GuideBadge extends TaingElement {
       >
       <span class="badge type-txt red">Quick VOD</span>
       <span class="badge type-txt green">무료</span>
+    `;
+  }
+}
+
+@customElement('t-guide')
+class Guide extends TaingElement {
+  static styles = css`
+    .guide-wrap {
+      padding: 2rem;
+
+      h1 {
+        display: flex;
+        align-items: center;
+        column-gap: 0.5rem;
+        padding-block-end: 2rem;
+        font-size: 1.6rem;
+        color: #d3d3d3;
+
+        img {
+          inline-size: 108px;
+        }
+      }
+
+      .guide-title {
+        padding-block-end: 1rem;
+        font-size: 1.5rem;
+        font-weight: 900;
+        color: #e93945;
+
+        & ~ .guide-title {
+          margin-block-start: 3rem;
+        }
+      }
+
+      .component-wrap {
+        padding-bottom: 2rem;
+
+        &.flex {
+          display: flex;
+          flex-flow: column wrap;
+          row-gap: 1rem;
+        }
+      }
+    }
+  `;
+
+  render() {
+    return html`
+      <div class="guide-wrap">
+        <h1>
+          <img src="/assets/images/logo/logo.svg" class="logo" alt="TAING" />
+          style guide
+        </h1>
+
+        <h2 class="guide-title">🪄 Logo</h2>
+        <guide-logo></guide-logo>
+
+        <h2 class="guide-title">🪄 Badge</h2>
+        <guide-badge></guide-badge>
+
+        <h2 class="guide-title">🪄 Form</h2>
+        <div class="component-wrap">
+          <login-checkbox>label</login-checkbox>
+          <t-checkbox>label</t-checkbox>
+        </div>
+        <t-input>
+          <label slot="label">아이디</label>
+          <strong slot="hint" class="hint"
+            >영문 또는 영문, 숫자 조합 6~12자리</strong
+          >
+        </t-input>
+        <t-input type="password">
+          <label slot="label">비밀번호</label>
+          <strong slot="hint" class="hint"
+            >영문, 숫자, 특수문자(~!@#$%^&*) 조합 8~15자리</strong
+          >
+        </t-input>
+        <t-input type="email">
+          <label slot="label">이메일</label>
+        </t-input>
+
+        <h2 class="guide-title">🪄 Buttons</h2>
+        <div class="component-wrap">
+          <guide-buttons></guide-buttons>
+        </div>
+
+        <div class="component-wrap flex">
+          <t-button></t-button>
+          <t-button type="submit" color="primary">로그인</t-button>
+          <t-button color="secondary">본인인증</t-button>
+          <t-button color="line">프로필 편집</t-button>
+        </div>
+
+        <h2 class="guide-title">🪄 Modal</h2>
+        <!--t-modal
+          popup
+          class="popup"
+          .cancelFn=${() => {
+          const modal = this.shadowRoot?.querySelector('.popup') as HTMLElement;
+
+          if (modal) {
+            modal.hidden = true;
+          }
+        }}
+        ></t-modal-->
+        <t-modal
+          class="modal-confirm"
+          .confirmFn=${() => console.log('컨펌 콜백')}
+          .cancelFn=${() => {
+            const modal = this.shadowRoot?.querySelector(
+              '.modal-confirm'
+            ) as HTMLElement;
+
+            if (modal) {
+              modal.hidden = true;
+            }
+          }}
+          >로그아웃 하시겠습니까?</t-modal
+        >
+      </div>
     `;
   }
 }
---
+++ src/components/Modal.ts
@@ -0,0 +1,173 @@
+import { customElement, property } from 'lit/decorators.js';
+import { TaingElement } from './Taing';
+import { CSSResultGroup, html, css, nothing } from 'lit';
+
+@customElement('t-modal')
+class Modal extends TaingElement {
+  static styles: CSSResultGroup = [
+    super.styles,
+    css`
+      .modal {
+        --popup-btn-wrap-padding: var(--size-2);
+        --popup-btn-font-size: var(--text-size-s);
+        --popup-btn-padding: var(--size-2);
+        --popup-btn-gap: var(--size-5);
+
+        --modal-min-width: 12.25rem;
+        --modal-padding-inline: 1.5625rem;
+        --modal-padding: var(--size-5) var(--modal-padding-inline) var(--size-3);
+        --modal-btn-padding: 0.625rem;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: fixed;
+        inset: 0;
+        z-index: 9999;
+
+        &:has(.modal__popup) {
+          background-color: #19191990;
+          backdrop-filter: var(--header-filter);
+        }
+
+        @media (min-width: 48rem) {
+          --modal-padding-inline: 3.125rem;
+          --modal-padding: var(--size-7) var(--modal-padding-inline)
+            var(--size-4);
+          --modal-min-width: 18.4375rem;
+          --modal-btn-padding: 0.875rem;
+        }
+        @media (min-width: 120rem) {
+          --popup-btn-padding: var(--size-4);
+          --popup-btn-font-size: var(--text-size-l);
+          --popup-btn-padding: var(--size-4);
+          --modal-min-width: 32.875rem;
+        }
+      }
+
+      .modal-popup__wrap {
+        display: flex;
+        column-gap: var(--popup-btn-gap);
+        padding-inline: var(--popup-btn-wrap-padding);
+      }
+
+      .modal-popup__btn {
+        flex: 1;
+        padding: var(--popup-btn-padding) 0;
+        border: 0;
+        background-color: initial;
+        font-size: var(--popup-btn-font-size);
+        line-height: 1.6;
+        color: inherit;
+        white-space: nowrap;
+        appearance: none;
+        cursor: pointer;
+
+        &.today {
+          flex: 1.605263157894737;
+        }
+      }
+
+      .modal__inner {
+        min-inline-size: var(--modal-min-width);
+        background-color: var(--dark-bg-2);
+        border-radius: var(--round-xs);
+
+        .modal__btn-wrap {
+          display: flex;
+          padding-inline: var(--modal-padding-inline);
+
+          .modal__btn {
+            flex: 1;
+            padding-block: var(--modal-btn-padding);
+            border: 0;
+            background-color: initial;
+            color: inherit;
+            appearance: none;
+            cursor: pointer;
+          }
+        }
+      }
+
+      .modal__message {
+        padding: var(--modal-padding);
+        color: var(--white);
+        text-align: center;
+      }
+    `,
+  ];
+
+  @property({ type: Boolean }) popup = false;
+  @property({ attribute: false }) confirmFn?: () => void;
+  @property({ attribute: false }) cancelFn?: () => void;
+
+  connectedCallback(): void {
+    super.connectedCallback();
+
+    this.bodyEl.classList.add('fixed');
+  }
+
+  bodyEl = document.querySelector<HTMLElement>('html')!;
+
+  handleConfirm() {
+    if (this.confirmFn) {
+      this.confirmFn();
+    }
+  }
+
+  handleCancel() {
+    this.bodyEl.classList.remove('fixed');
+
+    if (this.cancelFn) {
+      this.cancelFn();
+    }
+  }
+
+  render() {
+    return html`
+      <div class="modal">
+        ${this.popup
+          ? html`<div class="modal__popup">
+              <figure class="modal__img">
+                <img src="" alt="" />
+              </figure>
+              <div class="modal-popup__wrap">
+                <button type="button" class="modal-popup__btn today">
+                  오늘 하루 보지 않기
+                </button>
+                <button
+                  type="button"
+                  class="modal-popup__btn"
+                  @click=${this.handleCancel}
+                >
+                  닫기
+                </button>
+              </div>
+            </div>`
+          : html`
+              <div class="modal__inner">
+                <p class="modal__message">
+                  <slot></slot>
+                </p>
+                <div class="modal__btn-wrap">
+                  <button
+                    type="button"
+                    class="modal__btn"
+                    @click=${this.handleConfirm}
+                  >
+                    확인
+                  </button>
+                  <button
+                    type="button"
+                    class="modal__btn"
+                    @click=${this.handleCancel}
+                  >
+                    취소
+                  </button>
+                </div>
+              </div>
+            `}
+      </div>
+    `;
+  }
+}

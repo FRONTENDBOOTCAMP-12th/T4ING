bc6fbf9226ac1bb74d1e8df56d92227418795069
hyerim <110741@daum.net>
2024-12-29 19:23:00 +0900

    Design: 메인 추천콘텐츠 CSS
    - css 파일 컴포넌트와 분리
    - 이미지 아래 제목 추가, 네비게이션 버튼 추가
    - css는 계속 수정중
    
    #6

--- src/components/main/MainRecommend.ts
+++ src/components/main/MainRecommend.ts
@@ -4,6 +4,7 @@ import { customElement, property } from 'lit/decorators.js';
 import { html, css, CSSResultGroup } from 'lit';
 import { getRecommendImageURL } from '../../api/getMainPageURL';
 import Swiper from 'swiper';
+import mainRecommendCSS from '../../styles/mainRecommendCSS';
 
 interface SwiperContainerElement extends HTMLElement {
   swiper: Swiper;
@@ -25,109 +26,7 @@ class MainRecommend extends TaingElement {
   @property({ type: Boolean }) isBeginning = true;
   @property({ type: Boolean }) isEnd = false;
 
-  static styles: CSSResultGroup = [
-    super.styles,
-    css`
-      .container {
-        position: relative;
-        display: flex;
-        flex-direction: column;
-        width: 100%;
-        min-width: 320px;
-        height: auto;
-        gap: 1.2rem;
-        padding-inline: 0.5rem;
-        margin-block: 1.2rem;
-
-        box-sizing: border-box;
-        overflow: clip;
-        overflow-clip-box: padding-box;
-
-        background-color: transparent;
-
-        @media (min-width: 768px) {
-          padding-inline: 2.5rem;
-        }
-        @media (min-width: 1920px) {
-          padding-inline: 4.375rem;
-        }
-
-        & h1 {
-          font-size: 0.75rem;
-          font-weight: 600;
-          color: var(--white);
-
-          @media (min-width: 768px) {
-            font-size: 1rem;
-            font-weight: 700;
-          }
-          @media (min-width: 1920px) {
-            font-size: 1.7769rem;
-            font-weight: 700;
-          }
-        }
-      }
-
-      swiper-container {
-        position: relative;
-        inline-size: 100%;
-        margin-inline: 0;
-
-        @media (max-width: 767px) {
-          width: calc((100% - 16px));
-
-          &.is-middle {
-            margin-inline: 0.5rem;
-          }
-          &.is-end {
-            transform: translateX(1rem);
-          }
-        }
-
-        overflow: visible;
-
-        box-sizing: border-box;
-      }
-      ::part(container) {
-        inline-size: 100%;
-        box-sizing: border-box;
-
-        overflow: visible;
-      }
-
-      ::part(wrapper) {
-        inline-size: 100%;
-        position: relative;
-        box-sizing: border-box;
-        overflow: visible;
-      }
-
-      swiper-slide {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        inline-size: 100%;
-        block-size: auto;
-        transition: transform 0.2s ease-in-out;
-
-        & img {
-          inline-size: 100%;
-          block-size: auto;
-          object-fit: cover;
-          border-radius: 0.5rem;
-        }
-      }
-
-      swiper-slide:hover {
-        transform: translateY(-0.3rem);
-      }
-
-      .dimmed-slide {
-        opacity: 0.3;
-        transition: opacity 0.3s ease;
-      }
-    `,
-  ];
+  static styles: CSSResultGroup = [super.styles, mainRecommendCSS];
 
   get swiperContainer(): SwiperContainerElement | null {
     return this.renderRoot.querySelector(
@@ -141,14 +40,26 @@ class MainRecommend extends TaingElement {
 
   connectedCallback() {
     super.connectedCallback();
-    window.addEventListener('resize', this.handleResize);
-
     this.fetchData();
   }
 
-  disconnectedCallback() {
-    super.disconnectedCallback();
-    window.removeEventListener('resize', this.handleResize);
+  firstUpdated() {
+    if (this.swiperContainer) {
+      // Swiper 인스턴스가 초기화될 때까지 기다리기
+      if (!this.swiperInstance) {
+        this.swiperContainer.addEventListener('swiper-init', (event: Event) => {
+          this.handleResize();
+          this.handleSlideState();
+          this.attachNavigation();
+          window.addEventListener('resize', this.handleResize);
+        });
+      } else {
+        this.handleResize();
+        this.handleSlideState();
+        this.attachNavigation();
+        window.addEventListener('resize', this.handleResize);
+      }
+    }
   }
 
   handleResize = () => {
@@ -157,7 +68,6 @@ class MainRecommend extends TaingElement {
       this.device = newDevice;
       console.log('rec-device-changed:', this.device);
     }
-    this.toggleDimSlide();
   };
 
   async fetchData() {
@@ -167,155 +77,115 @@ class MainRecommend extends TaingElement {
       );
       const data = await response.json();
       this.data = data;
-      console.log('rec-device :', this.device);
-
-      this.addSwiperEvents();
     } catch (error) {
       console.error('Error fetching recommend data:', error);
     }
   }
 
-  addSwiperEvents() {
+  handleSlideState() {
     const swiper = this.swiperInstance;
     if (swiper) {
-      swiper.on('slideChange', () => {
-        this.updateSwiperState();
-        this.toggleDimSlide();
-      });
-      swiper.on('reachBeginning', this.updateSwiperState);
-      swiper.on('reachEnd', this.updateSwiperState);
-    }
-  }
-
-  removeSwiperEvents() {
-    const swiper = this.swiperInstance;
-    if (swiper) {
-      swiper.off('slideChange', () => {
-        this.updateSwiperState();
-        this.toggleDimSlide();
-      });
-      swiper.off('reachBeginning', this.updateSwiperState);
-      swiper.off('reachEnd', this.updateSwiperState);
-    }
-  }
-
-  updateSwiperState = () => {
-    const swiper = this.swiperInstance;
-    if (swiper) {
-      const isBeginningChanged = this.isBeginning !== swiper.isBeginning;
-      const isEndChanged = this.isEnd !== swiper.isEnd;
-
-      if (isBeginningChanged || isEndChanged) {
+      this.swiperInstance.on('slideChange', () => {
         this.isBeginning = swiper.isBeginning;
         this.isEnd = swiper.isEnd;
-
-        const swiperContainer =
-          this.renderRoot.querySelector('swiper-container');
-        if (swiperContainer) {
-          swiperContainer.classList.toggle('is-beginning', this.isBeginning);
-          swiperContainer.classList.toggle('is-end', this.isEnd);
-        }
-
-        console.log('swiper-state:', this.isBeginning, this.isEnd);
-
-        this.requestUpdate();
-
-        this.toggleDimSlide();
-      }
-    }
-  };
-
-  toggleDimSlide() {
-    const swiper = this.swiperInstance;
-    const device = this.device;
-    const slides = swiper?.slides;
-    if (!slides) return;
-    if (device === 'mobile' && slides.length > 0) {
-      slides.forEach((slide: HTMLElement, index: number) => {
-        slide.classList.remove('dimmed-slide');
-        if (
-          index === swiper.activeIndex - 1 ||
-          index === swiper.activeIndex + 3
-        )
-          slide.classList.add('dimmed-slide');
-      });
-    } else if (device === 'tablet' && slides.length > 0) {
-      slides.forEach((slide: HTMLElement, index: number) => {
-        slide.classList.remove('dimmed-slide');
-        if (
-          index === swiper.activeIndex - 1 ||
-          index === swiper.activeIndex + 5
-        )
-          slide.classList.add('dimmed-slide');
-      });
-    } else if (device === 'desktop' && slides.length > 0) {
-      slides.forEach((slide: HTMLElement, index: number) => {
-        slide.classList.remove('dimmed-slide');
-        if (
-          index === swiper.activeIndex - 1 ||
-          index === swiper.activeIndex + 7
-        )
-          slide.classList.add('dimmed-slide');
       });
     }
-    this.requestUpdate();
+  }
+
+  attachNavigation() {
+    // 커스텀 네비게이션 버튼 요소 선택
+    const prevButton = this.renderRoot.querySelector('.prev-btn');
+    const nextButton = this.renderRoot.querySelector('.next-btn');
+
+    if (this.swiperInstance && prevButton && nextButton) {
+      prevButton.addEventListener('click', () => {
+        this.swiperInstance?.slidePrev();
+      });
+
+      nextButton.addEventListener('click', () => {
+        this.swiperInstance?.slideNext();
+      });
+    }
   }
 
   render() {
     return html`
       <div class="container">
         <h1>티빙에서 꼭 봐야하는 콘텐츠</h1>
-        <swiper-container
-          class="${this.isBeginning
-            ? 'is-beginning'
-            : this.isEnd
-              ? 'is-end'
-              : 'is-middle'}"
-          .slidesPerView=${3}
-          .slidesPerGroup=${3}
-          .spaceBetween=${8}
-          .observer=${true}
-          .observeParents=${true}
-          .breakpoints=${{
-            768: {
-              slidesPerView: 5,
-              slidesPerGroup: 5,
-              spaceBetween: 8,
-            },
-            1920: {
-              slidesPerView: 7,
-              slidesPerGroup: 7,
-              spaceBetween: 8,
-            },
-          }}
-        >
-          ${this.data.items
-            .filter((item) => item.device === this.device)
-            .sort((a, b) => a.title.localeCompare(b.title))
-            .map(
-              (slide) => html`
-                <swiper-slide>
-                  <img
-                    src="${getRecommendImageURL(slide)}"
-                    alt="${slide.title}"
-                  />
-                </swiper-slide>
-              `
-            )}
-          ${this.data.items
-            .filter((item) => item.device === this.device)
-            .sort((a, b) => a.title.localeCompare(b.title))
-            .map(
-              (slide) => html`
-                <swiper-slide>
-                  <img
-                    src="${getRecommendImageURL(slide)}"
-                    alt="${slide.title}"
-                  />
-                </swiper-slide>
-              `
-            )}
-        </swiper-container>
+        <div class="swiper-outer-wrapper">
+          <button
+            class="nav-btn prev-btn"
+            ?disabled=${this.isBeginning}
+            aria-label="Go to previous slide"
+          ></button>
+          <button
+            class="nav-btn next-btn"
+            ?disabled=${this.isEnd}
+            aria-label="Go to next slide"
+          ></button>
+          <swiper-container
+            class="${this.isBeginning
+              ? 'is-beginning'
+              : this.isEnd
+                ? 'is-end'
+                : 'is-middle'}"
+            .slidesPerView=${3}
+            .slidesPerGroup=${3}
+            .spaceBetween=${8}
+            .observer=${true}
+            .observeParents=${true}
+            .breakpoints="${{
+              768: {
+                slidesPerView: 5,
+                slidesPerGroup: 5,
+                spaceBetween: 8,
+              },
+              1920: {
+                slidesPerView: 7,
+                slidesPerGroup: 7,
+                spaceBetween: 8,
+              },
+            }}"
+          >
+            ${this.data.items
+              .filter((item) => item.device === this.device)
+              .sort((a, b) => a.title.localeCompare(b.title))
+              .map(
+                (slide) => html`
+                  <swiper-slide tabindex="0">
+                    <figure class="slide-img-container">
+                      <img
+                        src="${getRecommendImageURL(slide)}"
+                        alt="${slide.title}"
+                      />
+                      <figcaption class="slide-title">
+                        ${slide.title}
+                      </figcaption>
+                    </figure>
+                  </swiper-slide>
+                `
+              )}
+            ${this.data.items
+              .filter((item) => item.device === this.device)
+              .sort((a, b) => a.title.localeCompare(b.title))
+              .map(
+                (slide) => html`
+                  <swiper-slide tabindex="0">
+                    <figure class="slide-img-container">
+                      <img
+                        class="slide-img"
+                        src="${getRecommendImageURL(slide)}"
+                        alt="${slide.title}"
+                      />
+                      <figcaption class="slide-title">
+                        ${slide.title}
+                      </figcaption>
+                    </figure>
+                  </swiper-slide>
+                `
+              )}
+          </swiper-container>
+        </div>
       </div>
     `;
   }

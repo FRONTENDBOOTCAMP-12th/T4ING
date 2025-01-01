6596fbc879daff868825837640a1d4bc38eeb59c
혜미 <tomboy.kr@gmail.com>
2025-01-01 19:02:28 +0900

    Modify: 랜딩 타이틀 css 반응형 수정
    - 타이틀 css 반응형 수정
    
    #5

--- src/styles/landingPanoramaCSS.ts
+++ src/styles/landingPanoramaCSS.ts
@@ -2,9 +2,12 @@ import { css } from 'lit';
 
 export default css`
   .panorama-container {
-    --title-font-size: var(--text-size-m);
-    --primary-font-size: var(--text-size-s);
-    --secondary-font-size: var(--text-size-s);
+    --container-padding: 5rem;
+    --title-font-size: var(--text-size-l);
+    --description-margin: 0;
+    --description-padding: 1.625rem;
+    --description-font-size: var(--text-size-s);
+    --description-sub-margin: 0;
     --image-width: 11.25rem;
     --image-height: 6.375rem;
     --slide-margin: var(--size-3);
@@ -13,27 +16,49 @@ export default css`
     width: 100%;
     text-align: center;
     color: white;
-    padding-block: 4rem;
+    padding-block-start: var(--container-padding);
     margin-bottom: 2rem;
     outline: none;
 
+    @media (min-width: 48rem) {
+      --title-font-size: var(--text-size-xl);
+      --description-padding: var(--size-8);
+      --description-font-size: var(--text-size-m);
+      --description-sub-font-size: var(--text-size-s);
+    }
+    @media (min-width: 120rem) {
+      --container-padding: 12.5rem;
+      --slide-container-height: 50rem;
+
+      --title-font-size: var(--text-size-3xl);
+      --description-margin: var(--size-5);
+      --description-padding: 3.125rem;
+      --description-font-size: var(--text-size-xl);
+      --description-sub-margin: var(--size-2);
+      --description-sub-font-size: var(--text-size-l);
+    }
+
     .panorama-title {
-      font-size: var(--title-font-size);
       margin-bottom: 0.625rem;
-      line-height: 1.6;
+      font-size: var(--title-font-size);
+      font-weight: 700;
+      line-height: 1.4;
+      transition: 0.3s;
     }
 
-    .panorama-description-primary {
-      font-size: var(--primary-font-size);
-      margin-bottom: 1.25rem;
-      line-height: 1.6;
-    }
+    .panorama-description {
+      margin-block-start: var(--description-margin);
+      padding-block-end: var(--description-padding);
+      font-size: var(--description-font-size);
+      line-height: 1.5;
+      transition: 0.3s;
 
-    .panorama-description-secondary {
-      font-size: var(--secondary-font-size);
-      margin-bottom: 1.25rem;
-      line-height: 1.6;
-      visibility: hidden;
+      span {
+        display: block;
+        margin-block-start: var(--description-sub-margin);
+        font-size: var(--description-sub-font-size);
+        line-height: 1.6;
+      }
     }
 
     .panorama-slider {
--- src/styles/landingSlideCSS.ts
+++ src/styles/landingSlideCSS.ts
@@ -4,35 +4,65 @@ export default css`
   .slide-container {
     --slide-container-min-width: 20rem;
     --slide-container-height: 30rem;
+    --slide-container-padding: 3rem;
 
-    --title-font-size: var(--size-5);
-    --primary-font-size: var(--size-3);
-    --secondary-font-size: var(--size-3);
+    --title-font-size: var(--text-size-l);
+    --description-margin: 0;
+    --description-padding: 1.625rem;
+    --description-font-size: var(--text-size-s);
+    --description-sub-margin: 0;
 
     position: relative;
     min-width: var(--slide-container-min-width);
     width: 100%;
     height: var(--slide-container-height);
     margin: 0 auto;
+    padding-block-start: var(--slide-container-padding);
     aspect-ratio: 4/5;
+
+    @media (min-width: 48rem) {
+      --slide-container-padding: 2.5rem;
+
+      --title-font-size: var(--text-size-xl);
+      --description-padding: var(--size-8);
+      --description-font-size: var(--text-size-m);
+      --description-sub-font-size: var(--text-size-s);
+    }
+    @media (min-width: 120rem) {
+      --slide-container-height: 50rem;
+      --slide-container-padding: 10rem;
+
+      --title-font-size: var(--text-size-3xl);
+      --description-margin: var(--size-5);
+      --description-padding: 3.125rem;
+      --description-font-size: var(--text-size-xl);
+      --description-sub-margin: var(--size-2);
+      --description-sub-font-size: var(--text-size-l);
+    }
 
     .slide-title {
       font-size: var(--title-font-size);
+      font-weight: 700;
       text-align: center;
       color: var(--white);
-      line-height: 1.6;
+      line-height: 1.4;
+      transition: 0.3s;
     }
-    .slide-description-primary {
-      font-size: var(--primary-font-size);
+    .slide-description {
+      margin-block-start: var(--description-margin);
+      padding-block-end: var(--description-padding);
+      font-size: var(--description-font-size);
       text-align: center;
       color: var(--gray200);
-      line-height: 1.6;
-    }
-    .slide-description-secondary {
-      font-size: var(--secondary-font-size);
-      text-align: center;
-      color: var(--gray200);
-      line-height: 1.6;
+      line-height: 1.5;
+      transition: 0.3s;
+
+      span {
+        display: block;
+        margin-block-start: var(--description-sub-margin);
+        font-size: var(--description-sub-font-size);
+        line-height: 1.6;
+      }
     }
 
     .slider-container {
@@ -49,22 +79,6 @@ export default css`
 
     .landing-link:focus {
       outline: 2px solid var(--focus-color);
-    }
-    @media (min-width: 48rem) {
-      --primary-font-size: var(--size-4);
-      .slide-title {
-        visibility: hidden;
-      }
-    }
-    @media (min-width: 120rem) {
-      --title-font-size: var(--size-13);
-      --primary-font-size: var(--size-7);
-      --secondary-font-size: var(--size-5);
-
-      --slide-container-height: 50rem;
-      .slide-title {
-        visibility: visible;
-      }
     }
   }
 `;

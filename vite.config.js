71255bb9bd5f7d12cda35032c00ec3d2d2de388e
혜미 <tomboy.kr@gmail.com>
2025-01-02 10:49:49 +0900

    Modify: og 태그 수정
    - og 태그 description 수정
    
    #8

--- vite.config.js
+++ vite.config.js
@@ -43,12 +43,12 @@ export default defineConfig({
           '</head>',
           `
     <meta name="author" content="TAING">
-    <meta name="description" content="멋쟁이 사자처럼 태킷 프론트엔드 스쿨 12기 바닐라 프로젝트 4조 4인머스캣의 바닐라 프로젝트 웹사이트.">
+    <meta name="description" content="멋쟁이 사자처럼 태킷 프론트엔드 스쿨 12기 바닐라 프로젝트 4조 4인머스캣의 타잉 웹사이트.">
     <meta name="keywords" content="멋쟁이 사자처럼, 프론트엔드 스쿨">
     <meta property="og:site_name" content="TAING">
     <meta property="og:type" content="website">
     <meta property="og:title" content="TAING">
-    <meta property="og:description" content="멋쟁이 사자처럼 태킷 프론트엔드 스쿨 12기 바닐라 프로젝트 4조 4인머스캣의 바닐라 프로젝트 웹사이트.">
+    <meta property="og:description" content="멋쟁이 사자처럼 태킷 프론트엔드 스쿨 12기 바닐라 프로젝트 4조 4인머스캣의 타잉 웹사이트.">
     <meta property="og:image" content="/assets/images/og.png">
     <meta property="og:image:width" content="800">
     <meta property="og:image:height" content="400">

bec4211652223c08b16e7e4ef916670303e6c71d
혜미 <tomboy.kr@gmail.com>
2025-01-02 10:52:54 +0900

    Modify: index 화면 랜더링 분기 롤백
    - index 화면 랜더링 분기 롤백 (랜딩으로 리다이렉트)
    
    #6

--- src/components/main/Main.ts
+++ src/components/main/Main.ts
@@ -1,29 +1,36 @@
 import { html } from 'lit';
 import { customElement } from 'lit/decorators.js';
 import { TaingElement } from '../Taing';
-import { isLogin } from '../../utils/authUtils';
 import '../../components/main/MainModal';
 import '../../pages/main';
 import '../../pages/landing';
+import { isLogin } from '../../utils/authUtils';
 
 @customElement('main-page')
 class Main extends TaingElement {
+  connectedCallback(): void {
+    super.connectedCallback();
+
+    if (!isLogin()) {
+      location.href = '/src/pages/landing/';
+    }
+  }
+
   render() {
     return html`
-      ${isLogin()
-        ? html`
-            <div
-              class="home-container"
-              style="display: flex; flex-flow: column; gap: 3rem"
-            >
-              <main-banner></main-banner>
-              <main-recommend></main-recommend>
-              <main-vod></main-vod>
-              <main-top></main-top>
-            </div>
-            <main-modal open></main-modal>
-          `
-        : html`<landing-page></landing-page>`}
+      <div
+        class="home-container"
+        style="display: flex; flex-flow: column; gap: 4rem"
+      >
+        <main-banner></main-banner>
+        <main-recommend></main-recommend>
+        <main-vod></main-vod>
+        <main-top></main-top>
+        <main-live-channel></main-live-channel>
+        <main-t-original></main-t-original>
+        <main-bottom-banner></main-bottom-banner>
+        <main-event-banner></main-event-banner>
+      </div>
     `;
   }
 }

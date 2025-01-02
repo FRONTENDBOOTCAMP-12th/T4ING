54f57d4087faddd9bf4d4f145744a2203df9800d
Min-gyu <jorushs@gmail.com>
2025-01-03 02:08:45 +0900

    Modify: 얽혀있던 import문 수정
    - index.js 파일 import문 제거

--- src/components/main/Main.ts
+++ src/components/main/Main.ts
@@ -1,10 +1,16 @@
 import { html } from 'lit';
 import { customElement } from 'lit/decorators.js';
 import { TaingElement } from '../Taing';
-import '../../components/main/MainModal';
-import '../../pages/main';
-import '../../pages/landing';
 import { isLogin } from '../../utils/authUtils';
+import '../../components/main/MainModal';
+import '../../components/main/MainBanner';
+import '../../components/main/MainRecommend';
+import '../../components/main/MainVOD';
+import '../../components/main/MainTop';
+import '../../components/main/MainLiveChannel';
+import '../../components/main/MainToriginal';
+import '../../components/main/MainBottomBanner';
+import '../../components/main/MainEventBanner';
 
 @customElement('main-page')
 class Main extends TaingElement {
